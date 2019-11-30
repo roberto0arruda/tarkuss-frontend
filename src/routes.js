2ac78edfce22f2aebd56ec6b1dab1a0b6412d22e
Roberto Arruda <roberto0arruda@hotmail.com>
2019-11-29 23:04:58 -0400

    app build

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Login} />
+                <Route path="/home" component={Home} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
