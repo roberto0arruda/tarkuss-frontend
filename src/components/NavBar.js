27d93e16841f98868f811d493d8db73dac3bd8bc
Roberto Arruda <roberto0arruda@hotmail.com>
2019-12-04 09:53:10 -0400

    navbar position, dropdown animate

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,17 +15,22 @@ function NavBar() {
             <a href="/home" className="navbar-brand">
                 <img src={logoNavBar} alt="logo" />
             </a>
-            <ul className="navbar-nav top-right">
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src={userPlaceholder} width="30" height="30" className="d-inline-block align-right" alt="" />
-                        {user.username}
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a className="dropdown-item" href="#" onClick={handleLogOut}>Sign Out</a>
-                    </div>
-                </li>
-            </ul>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="##" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src={userPlaceholder} width="30" height="30" className="d-inline-block align-right" alt="" />
+                            {user.username}
+                        </a>
+                        <div className="dropdown-menu dropdown-menu-right animate slideIn" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="##" onClick={handleLogOut}>Sign Out</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
