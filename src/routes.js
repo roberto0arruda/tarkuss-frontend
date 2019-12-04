e87e08af3fc3ab348b94c5f6210076af8c5eec40
Roberto Arruda <roberto0arruda@hotmail.com>
2019-12-04 08:56:09 -0400

    route auth

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { isAuthenticated } from './auth';
 
 import Login from './pages/Login';
 import Home from './pages/Home';
 
 export default function Routes() {
+    const PrivateRoute = ({ component: Component, ...rest }) => (
+        <Route {...rest} render={props => (
+            isAuthenticated() ? (
+                <Component {...props} />
+            ) : (
+                    <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+                )
+        )
+        } />
+    );
+
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Login} />
-                <Route path="/home" component={Home} />
+                <PrivateRoute path="/home" component={Home} />
             </Switch>
         </BrowserRouter>
     );
